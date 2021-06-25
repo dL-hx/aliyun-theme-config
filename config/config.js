77841271bd3b41c88ca7f7f0fcce5bc48d345e41
dL-hx <897057246@qq.com>
2021-06-26 03:44:25 +0800

    first commit

---
+++ config/config.js
@@ -0,0 +1,124 @@
+// https://umijs.org/config/
+import { defineConfig } from 'umi';
+import defaultSettings from './defaultSettings';
+import proxy from './proxy';
+import routes from './routes';
+
+const { REACT_APP_ENV } = process.env;
+export default defineConfig({
+  hash: true,
+  antd: {
+	 
+  },
+  dva: {
+    hmr: true,
+  },
+  history: {
+    type: 'browser',
+  },
+  locale: {
+    // default zh-CN
+    default: 'zh-CN',
+    antd: true,
+    // default true, when it is true, will use `navigator.language` overwrite default
+    baseNavigator: true,
+  },
+  dynamicImport: {
+    loading: '@/components/PageLoading/index',
+  },
+  targets: {
+    ie: 11,
+  },
+  // umi routes: https://umijs.org/docs/routing
+  routes,
+  // Theme for antd: https://ant.design/docs/react/customize-theme-cn
+  theme: {
+     '@font-size-sm': '12px',
+  '@font-size-base': '12px',
+  '@primary-color': '#0070cc',
+  '@border-radius-base': '0',
+  '@border-radius-sm': '0',
+
+  '@text-color': 'fade(#000, 65%)',
+  '@text-color-secondary': 'fade(#000, 45%)',
+  '@background-color-base': 'hsv(0, 0, 96%)',
+
+  '@success-color': '#1e8e3e',
+  '@error-color': '#d93026',
+  '@warning-color': '#ffc440',
+  '@info-color': '@primary-color',
+  '@danger-color': '@error-color',
+  '@processing-color': '@primary-color',
+
+  '@border-color-base': '#dedede',
+  '@border-color-split': '#dedede',
+
+  '@outline-width': '0',
+  '@outline-color': '#737373',
+
+  '@input-height-lg': '36px',
+  '@input-height-base': '32px',
+  '@input-height-sm': '24px',
+  '@input-hover-border-color': '#737373',
+
+  '@form-item-margin-bottom': '16px',
+
+  '@btn-default-bg': '#fafafa',
+  '@btn-default-border': '#dedede',
+  '@btn-danger-color': '#fff',
+  '@btn-danger-bg': '@error-color',
+  '@btn-danger-border': '@error-color',
+
+  '@switch-color': '@success-color',
+
+  '@table-header-bg': '#fafafa',
+  '@table-row-hover-bg': '#fafafa',
+  '@table-padding-vertical': '15px',
+
+  '@badge-color': '@error-color',
+
+  '@breadcrumb-base-color': '@text-color',
+  '@breadcrumb-last-item-color': '@text-color-secondary',
+
+  '@slider-rail-background-color': '@background-color-base',
+  '@slider-rail-background-color-hover': '#e1e1e1',
+  '@slider-track-background-color': '@primary-color',
+  '@slider-track-background-color-hover': '@primary-color',
+  '@slider-handle-border-width': '1px',
+  '@slider-handle-color': '#dedede',
+  '@slider-handle-color-hover': '#dedede',
+  '@slider-handle-color-focus': '#dedede',
+  '@slider-handle-color-tooltip-open': '#ddd',
+  '@slider-handle-color-focus-shadow': 'transparent',
+  '@slider-handle-shadow': '1px 1px 4px 0 rgba(0,0,0,.13)',
+
+  '@alert-success-border-color': '#dff4e5',
+  '@alert-success-bg-color': '#dff4e5',
+  '@alert-info-border-color': '#e5f3ff',
+  '@alert-info-bg-color': '#e5f3ff',
+  '@alert-error-border-color': '#fcebea',
+  '@alert-error-bg-color': '#fcebea',
+  '@alert-warning-border-color': '#fff7db',
+  '@alert-warning-bg-color': '#fff7db',
+
+  '@radio-button-bg': 'transparent',
+  '@radio-button-checked-bg': 'transparent',
+
+  '@progress-radius': '0',
+
+  '@tabs-card-gutter': '-1px',
+  '@tabs-card-tab-active-border-top': '2px solid @primary-color',
+
+  '@layout-body-background': '#fafafa',
+  },
+  title: false,
+  ignoreMomentLocale: true,
+  proxy: proxy[REACT_APP_ENV || 'dev'],
+  manifest: {
+    basePath: '/',
+  },
+  // 快速刷新功能 https://umijs.org/config#fastrefresh
+  fastRefresh: {},
+  esbuild: {},
+  webpack5: {},
+});
---
+++ config/routes.js
@@ -0,0 +1,68 @@
+export default [
+  {
+    path: '/',
+    component: '../layouts/BlankLayout',
+    routes: [
+      {
+        path: '/user',
+        routes: [
+          {
+            name: 'login',
+            path: '/user/login',
+            component: './User/login',
+          },
+        ],
+      },
+      {
+        path: '/',
+        routes: [
+          {
+            path: '/',
+            component: '../layouts/BasicLayout',
+            routes: [
+              {
+                path: '/',
+                redirect: '/welcome',
+              },
+              {
+                path: '/welcome',
+                name: 'welcome',
+                icon: 'smile',
+                component: './Welcome',
+              },
+              {
+                path: '/admin',
+                name: 'admin',
+                icon: 'crown',
+                component: './Admin',
+                routes: [
+                  {
+                    path: '/admin/sub-page',
+                    name: 'sub-page',
+                    icon: 'smile',
+                    component: './Welcome',
+                  },
+                ],
+              },
+              {
+                name: 'list.table-list',
+                icon: 'table',
+                path: '/list',
+                component: './TableList',
+              },
+              {
+                component: './404',
+              },
+            ],
+          },
+          {
+            component: './404',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    component: './404',
+  },
+];
